ff3e2d5f0a621d500b1254160e41a8c16f1975b9
abdullah01 <abdullah@repairdesk.co>
2022-07-29 23:08:51 +0500

    add name in comment

--- models/announcement.model.js
+++ models/announcement.model.js
@@ -1,28 +1,29 @@
 const mongoose = require("mongoose");
 
 const announcementSchema = new mongoose.Schema(
-    {
+  {
+    user_id: { type: mongoose.Types.ObjectId, ref: "User" },
+    class_id: { type: mongoose.Types.ObjectId, ref: "Class" },
+    message: { type: String },
+    files: [{ type: String }],
+    comments: [
+      {
         user_id: { type: mongoose.Types.ObjectId, ref: "User" },
-        class_id: { type: mongoose.Types.ObjectId, ref: "Class" },
+        name: { type: String },
         message: { type: String },
-        files: [{ type: String, }],
-        comments: [
-            {
-                user_id: { type: mongoose.Types.ObjectId, ref: "User" },
-                name: { type: String, },
-                message: { type: String, },
-                files: [{ type: String, }],
-                // reply: [
-                //     {
-                //         user_id: { type: mongoose.Types.ObjectId, ref: "User" },
-                //         name: { type: String, },
-                //         message: { type: String, },
-                //     },
-                // ],
-            },
-        ],
-    },
-    { timestamps: true }
+        files: [{ type: String }],
+        time: { type: Date },
+        // reply: [
+        //     {
+        //         user_id: { type: mongoose.Types.ObjectId, ref: "User" },
+        //         name: { type: String, },
+        //         message: { type: String, },
+        //     },
+        // ],
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Announcement", announcementSchema);
