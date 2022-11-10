8e2bea87f864a39a15cb5e63588e519cdb0ec583
Muhammadumerishtiaq3 <muhammadumerishtiaq3@gmail.com>
2022-11-10 12:41:13 +0500

    HyperPay Integration

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,4 +1,8 @@
 const stripe = require("stripe")(process.env.secret_key);
+const https = require("https");
+const querystring = require("querystring");
+const axios = require("axios");
+const config = require("../config");
 
 // ---------------------------------------------------------------
 // --------------------- GET PUSBLISH KEY -----------------------------
@@ -24,13 +28,87 @@ exports.payment_intent = async (req, res, next) => {
       amount: updated_amount,
       currency: "usd",
       payment_method_types: ["card"],
-      metadata: { integration_check: "accept_a_payment" },
+      metadata: { integration_check: "accept_a_payment" }
     });
 
     res.send({
-      clientSecret: paymentIntent.client_secret,
+      clientSecret: paymentIntent.client_secret
     });
   } catch (error) {
     res.status(500).json({ message: error?.message });
   }
 };
+
+// ---------------------------------------------------------------
+// --------------------- CHECKOUT -----------------------------
+// ---------------------------------------------------------------
+exports.checkout = async (req, res, next) => {
+   request(function(responseData) {
+    res.json(responseData);
+  });
+};
+
+// ---------------------------------------------------------------
+// --------------------- CHECKOUT RESULT -----------------------------
+// ---------------------------------------------------------------
+exports.checkout_result = async (req, res, next) => {
+   resultRequest(req.body.resourcePath, function(responseData) {
+    res.json(responseData);
+  });
+};
+
+
+function request(callback) {
+  var path = "/v1/checkouts";
+  var data = querystring.stringify({
+    entityId: "8ac7a4c984364a1a01843d5034aa23da",
+    amount: "92.00",
+    currency: config.HYPERPAY_CURRENCY,
+    paymentType: config.HYPERPAY_PAYMENT_TYPE,
+  });
+  var options = {
+    port: 443,
+    host: "eu-test.oppwa.com",
+    path: path,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Length": data.length,
+      Authorization:
+        `Bearer ${config.HYPERPAY_BEARER_TOKEN}`,
+    },
+  };
+  var postRequest = https.request(options, function(res) {
+    res.setEncoding("utf8");
+    res.on("data", function(chunk) {
+      jsonRes = JSON.parse(chunk);
+      return callback(jsonRes);
+    });
+  });
+  postRequest.write(data);
+  postRequest.end();
+}
+
+function resultRequest(resourcePath, callback) {
+  var path = resourcePath;
+  path += "?entityId=8ac7a4c984364a1a01843d5034aa23da";
+  const url = "https://eu-test.oppwa.com" + path;
+  axios
+    .get(url, {
+      headers: {
+        Authorization:
+           `Bearer ${config.HYPERPAY_BEARER_TOKEN}`,
+      },
+    })
+    .then(function(response) {
+      try {
+        resDate = JSON.parse(response);
+      } catch (e) {
+        resData = response;
+      }
+      return callback(resData.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -2,11 +2,18 @@ const express = require("express");
 const sanitizeData = require("../middlewares/sanitize.js");
 const { auth } = require("../middlewares/auth.js");
 
-const { publish_key, payment_intent } = require("../controllers/payment.controller.js");
+const {
+  publish_key,
+  payment_intent,
+  checkout,
+  checkout_result
+} = require("../controllers/payment.controller.js");
 
 const router = express.Router();
 
 router.get("/publish-key", sanitizeData, auth, publish_key);
 router.post("/pay", sanitizeData, auth, payment_intent);
+router.get("/checkout", sanitizeData, checkout);
+router.get("/checkout-result", sanitizeData, checkout_result);
 
 module.exports = router;
